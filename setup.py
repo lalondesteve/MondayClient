9f2e22f5a00dbb7965933191c9a4f4c32a6b3733
lalondesteve <lalondesteve@gmail.com>
2020-09-03 00:33:46 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-import io, os
-from setuptools import setup
+# import os
+# import io
+import setuptools
 
 # Package meta-data.
 NAME = 'MondayClient'
@@ -13,37 +14,39 @@ REQUIRED = [
     'requests', 'wheel', 'python-dotenv'
 ]
 
-here = os.path.abspath(os.path.dirname(__file__))
+# here = os.path.abspath(os.path.dirname(__file__))
+#
+# # Import the README and use it as the long-description.
+# # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+# try:
+#     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+#         long_description = '\n' + f.read()
+# except FileNotFoundError:
+#     long_description = DESCRIPTION
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-
-setup(name=NAME,
-      version='0.1',
-      description=DESCRIPTION,
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url=URL,
-      author=AUTHOR,
-      author_email=EMAIL,
-      license='MIT',
-      packages=['MondayClient'],
-      install_requires=REQUIRED,
-      include_package_data=True,
-      zip_safe=False,
-      classifiers=[
-            # Trove classifiers
-            # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.8',
-            'Development Status :: 3 - Alpha',
-      ],
-      )
+setuptools. \
+    setup(name=NAME,
+          version='0.1',
+          description=DESCRIPTION,
+          long_description=long_description,
+          long_description_content_type='text/markdown',
+          url=URL,
+          author=AUTHOR,
+          author_email=EMAIL,
+          license='MIT',
+          packages=setuptools.find_packages(),
+          install_requires=REQUIRED,
+          include_package_data=True,
+          zip_safe=False,
+          classifiers=[
+              # Trove classifiers
+              # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.8',
+              'Development Status :: 3 - Alpha',
+          ],
+          )
